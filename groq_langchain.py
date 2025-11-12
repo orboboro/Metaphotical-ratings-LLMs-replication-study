0815fa2875b6adb0cff15157d3bb6859b42c48b7
orboboro <jacopo.1205@gmail.com>
2025-11-12 18:14:47 +0100

    c

---
+++ groq_langchain.py
@@ -0,0 +1,26 @@
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+client = Groq(
+
+    api_key=os.environ.get("GROQ_API_KEY"),
+)
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant."
+        },
+        {
+            "role": "user",
+            "content": "Explain the importance of fast language models",
+        }
+    ],
+    model="llama-3.3-70b-versatile",
+)
+
+print(chat_completion.choices[0].message.content)
+
